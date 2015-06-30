e8fbb6e2d8e116c7668e08cbcb79f1f2069defc1
wanghuafeng <18700821967@163.com>
2015-06-30 15:42:03 +0800

    Add Java File - World.java Create

---
+++ World.java
@@ -0,0 +1,8 @@
+public class Hello
+{
+	public static void main(String args[]){
+		System.out.println("www.mldnjava.com");
+		System.out.println("www.yootk.com");
+		System.out.println("Welcome Git!");
+	}
+}
