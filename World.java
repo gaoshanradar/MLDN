ab124b89f44d73948352cc46fb057c517f77acc6
wanghuafeng <18700821967@163.com>
2015-06-30 21:36:14 +0800

    change two files print content

--- World.java
+++ World.java
@@ -1,8 +1,6 @@
 public class Hello
 {
 	public static void main(String args[]){
-		System.out.println("www.mldnjava.com");
-		System.out.println("www.yootk.com");
-		System.out.println("Welcome Git!");
+		System.out.println("Hello World!");
 	}
 }
